bb8fe8fb3ef7cae21cdd742484c86782f7e0e851
TEH TING FENG <tingfeng@viddsee.com>
2023-04-08 00:58:32 +0800

    chore: update api key

--- stream-chat-android-ui-components-sample/src/demo/kotlin/io/getstream/chat/ui/sample/application/AppConfig.kt
+++ stream-chat-android-ui-components-sample/src/demo/kotlin/io/getstream/chat/ui/sample/application/AppConfig.kt
@@ -19,7 +19,7 @@ package io.getstream.chat.ui.sample.application
 import io.getstream.chat.ui.sample.data.user.SampleUser
 
 object AppConfig {
-    const val apiKey: String = "6nsccgxpbvzg"
+    const val apiKey: String = "8rqpszk7nyut"
     const val apiUrl: String = "chat-us-east-staging.stream-io-api.com"
     const val apiTimeout: Int = 6000
     const val cndTimeout: Int = 30000
